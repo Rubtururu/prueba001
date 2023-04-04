2d5b9838d9e2369cf92862dd3e2ff0eafc185a5d
Rubtururu <59855904+Rubtururu@users.noreply.github.com>
2023-04-04 08:40:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,199 +1,47 @@
-const contractAddress = "0x123abc..."; // Dirección del contrato
-const contractABI = [
-  // ABI del contrato
-  {
-    "inputs": [],
-    "stateMutability": "nonpayable",
-    "type": "constructor"
-  },
-  {
-    "inputs": [],
-    "name": "claimReward",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "creatorBalance",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "amount",
-        "type": "uint256"
-      }
-    ],
-    "name": "deposit",
-    "outputs": [],
-    "stateMutability": "payable",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "getCreatorBalance",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "getTreasuryBalance",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "amount",
-        "type": "uint256"
-      }
-    ],
-    "name": "setMaxDeposit",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "amount",
-        "type": "uint256"
-      }
-    ],
-    "name": "setMinDeposit",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "name": "users",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "balance",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint256",
-        "name": "lastClaimedTime",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "name": "withdraw",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
+const web3 = new Web3(window.ethereum);
+
+const contractAddress = "<contract_address>";
+const contractABI = <contract_abi>;
+
+const maticPool = new web3.eth.Contract(contractABI, contractAddress);
+
+async function depositMatic(event) {
+  event.preventDefault();
+  const depositAmount = document.getElementById("deposit-amount").value;
+  if (depositAmount <= 0) {
+    return alert("Amount should be greater than 0");
   }
-];
-
-// Crea una instancia del contrato con web3.js
-const provider = new ethers.providers.Web3Provider(web3.currentProvider);
-const signer = provider.getSigner();
-const contract = new ethers.Contract(contractAddress, contractABI, signer);
-
-// Actualiza el saldo de la pool del tesoro en el HTML
-async function updateTreasuryBalance() {
-  const balance = await contract.getTreasuryBalance();
-  const balanceElement = document.getElementById("treasuryBalance");
-  balanceElement.innerHTML = ethers.utils.formatEther(balance);
+  const accounts = await web3.eth.requestAccounts();
+  const from = accounts[0];
+  const value = web3.utils.toWei(depositAmount, "ether");
+  try {
+    await maticPool.methods.deposit().send({ from, value });
+    alert("Deposit successful!");
+    location.reload();
+  } catch (error) {
+    console.error(error);
+    alert("Deposit failed");
+  }
 }
 
-// Actualiza el saldo del usuario en el HTML
-async function updateUserBalance() {
-  const address = await signer.getAddress();
-  const userBalance = await contract.users(address);
-  const balanceElement = document.getElementById("userBalance");
-  balanceElement.innerHTML = ethers.utils.formatEther(userBalance.balance);
-}
-
-// Deposit function
-async function deposit(event) {
+async function withdrawMatic(event) {
   event.preventDefault();
-  const depositAmount = event.target.elements.depositAmount.value;
-  const transaction = await contract.deposit({
-    value: ethers.utils.parseEther(depositAmount)
-  });
-  await transaction.wait();
-  updateUserBalance();
-  updateTreasuryBalance();
+  const withdrawAmount = document.getElementById("withdraw-amount").value;
+  if (withdrawAmount <= 0) {
+    return alert("Amount should be greater than 0");
+  }
+  const accounts = await web3.eth.requestAccounts();
+  const from = accounts[0];
+  const value = web3.utils.toWei(withdrawAmount, "ether");
+  try {
+    await maticPool.methods.withdraw(value).send({ from });
+    alert("Withdrawal successful!");
+    location.reload();
+  } catch (error) {
+    console.error(error);
+    alert("Withdrawal failed");
+  }
 }
 
-// Withdraw function
-async function withdraw(event) {
-  event.preventDefault();
-  const withdrawAmount = event.target.elements.withdrawAmount.value;
-  const transaction = await contract.withdraw(
-    ethers.utils.parseEther(withdrawAmount)
-  );
-  await transaction.wait();
-  updateUserBalance();
-  updateTreasuryBalance();
-}
-
-// Claim reward function
-async function claimReward() {
-  const transaction = await contract.claimReward();
-  await transaction.wait();
-  updateUserBalance();
-  updateTreasuryBalance();
-}
-
-// Actualiza los saldos al cargar la página
-window.onload = async function () {
-  await updateTreasuryBalance();
-  await updateUserBalance();
-
-  // Agrega los event listeners para deposit y withdraw
-  const depositForm = document.getElementById("depositForm");
-  depositForm.addEventListener("submit", deposit);
-  const withdrawForm = document.getElementById("withdrawForm");
-  withdrawForm.addEventListener("submit", withdraw);
-
-  // Agrega el event listener para el botón de claimReward
-  const claimRewardButton = document.getElementById("claimRewardButton");
-  claimRewardButton.addEventListener("click", claimReward);
-};
+async function claimDividend() {
+  const accounts =
