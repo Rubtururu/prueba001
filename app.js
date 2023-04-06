4510a1ac345210c95d5fd8d1762e7312f0574e1f
Rubtururu <59855904+Rubtururu@users.noreply.github.com>
2023-04-07 00:30:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ window.addEventListener('load', async () => {
       const accounts = await web3.eth.getAccounts();
       if (accounts.length > 0) {
         const account = accounts[0];
-        const contractAddress = '0x4b8eE4A9D2bC11F4d4580d83F7b2C2B0C7c0C01B';
+        const contractAddress = '0xB8df7EEc5F3B4136D5BE3024d445A7493c274B26';
         const contract = new web3.eth.Contract(abi, contractAddress);
         document.getElementById('balance').textContent = await contract.methods.totalDeposits().call({ from: account });
         document.getElementById('treasury').textContent = await web3.eth.getBalance(contractAddress);
