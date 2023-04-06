adb0d7d075b76a66bfc4f48d1ab5a284a7aa82e7
Rubtururu <59855904+Rubtururu@users.noreply.github.com>
2023-04-07 00:03:58 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,112 +1,26 @@
-// Reemplaza los siguientes valores con los detalles de tu contrato
-const contractABI = [
+// Conectar a la red Ethereum usando Web3.js
+var web3 = new Web3(Web3.givenProvider);
+
+// Obtener la instancia del contrato
+var contract = new web3.eth.Contract([
 	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "bidAmount",
-				"type": "uint256"
-			}
-		],
-		"name": "bid",
+		"inputs": [],
+		"name": "claimRewards",
 		"outputs": [],
 		"stateMutability": "nonpayable",
 		"type": "function"
 	},
 	{
 		"inputs": [],
-		"name": "claimReward",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			}
-		],
 		"name": "deposit",
 		"outputs": [],
-		"stateMutability": "nonpayable",
+		"stateMutability": "payable",
 		"type": "function"
 	},
 	{
 		"inputs": [],
-		"name": "distributeAuctionRewards",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "_usdtToken",
-				"type": "address"
-			}
-		],
 		"stateMutability": "nonpayable",
 		"type": "constructor"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "user",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			}
-		],
-		"name": "Deposit",
-		"type": "event"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "user",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "bidAmount",
-				"type": "uint256"
-			}
-		],
-		"name": "NewBid",
-		"type": "event"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "user",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			}
-		],
-		"name": "Reward",
-		"type": "event"
 	},
 	{
 		"inputs": [
@@ -122,27 +36,8 @@ const contractABI = [
 		"type": "function"
 	},
 	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "user",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			}
-		],
-		"name": "Withdraw",
-		"type": "event"
-	},
-	{
 		"inputs": [],
-		"name": "auctionEndTime",
+		"name": "DISTRIBUTION_INTERVAL",
 		"outputs": [
 			{
 				"internalType": "uint256",
@@ -155,70 +50,7 @@ const contractABI = [
 	},
 	{
 		"inputs": [],
-		"name": "auctionPool",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "auctionStartTime",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "auctionTopBidders",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "user",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "bidAmount",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "lastBid",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "lastRewardTime",
+		"name": "lastDistribution",
 		"outputs": [
 			{
 				"internalType": "uint256",
@@ -244,7 +76,7 @@ const contractABI = [
 	},
 	{
 		"inputs": [],
-		"name": "treasuryPool",
+		"name": "OWNER_FEE",
 		"outputs": [
 			{
 				"internalType": "uint256",
@@ -257,12 +89,38 @@ const contractABI = [
 	},
 	{
 		"inputs": [],
-		"name": "usdtToken",
+		"name": "totalDeposits",
 		"outputs": [
 			{
-				"internalType": "contract IERC20",
+				"internalType": "uint256",
 				"name": "",
-				"type": "address"
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "TREASURY_FEE",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "treasuryPool",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
 			}
 		],
 		"stateMutability": "view",
@@ -285,118 +143,66 @@ const contractABI = [
 			},
 			{
 				"internalType": "uint256",
-				"name": "lastRewardTime",
+				"name": "lastClaim",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "WITHDRAWAL_FEE",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
 				"type": "uint256"
 			}
 		],
 		"stateMutability": "view",
 		"type": "function"
 	}
-] // Incluir el ABI del contrato aquí
-const contractAddress = "0x2ba53e39A92E70c9f2e3ca53446f260A74047045"; // Incluir la dirección del contrato aquí
+], 0xB8df7EEc5F3B4136D5BE3024d445A7493c274B26);
 
-let web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
-let contract = new web3.eth.Contract(contractABI, contractAddress);
-
-async function updateStats() {
-    const accounts = await web3.eth.getAccounts();
-    const userAddress = accounts[0];
-
-    // Balance del usuario
-    const userBalance = await contract.methods.balanceOf(userAddress).call();
-    document.getElementById('userBalance').innerText = `${web3.utils.fromWei(userBalance, 'ether')} USDT`;
-
-    // Pool del Tesoro
-    const treasuryPool = await contract.methods.getTreasuryPool().call();
-    document.getElementById('treasuryPool').innerText = `${web3.utils.fromWei(treasuryPool, 'ether')} USDT`;
-
-    // Pool de Subasta
-    const auctionPool = await contract.methods.getAuctionPool().call();
-    document.getElementById('auctionPool').innerText = `${web3.utils.fromWei(auctionPool, 'ether')} USDT`;
+// Función para obtener y mostrar la dirección del propietario del contrato
+function getOwner() {
+  contract.methods.owner().call().then(function(result) {
+    console.log("Owner: " + result);
+  });
 }
 
-async function deposit() {
-    const amount = document.getElementById('depositAmount').value;
-    const accounts = await web3.eth.getAccounts();
-
-    contract.methods.deposit(web3.utils.toWei(amount, 'ether')).send({ from: accounts[0] })
-        .on('transactionHash', (hash) => {
-            console.log('Transacción enviada:', hash);
-        })
-        .on('receipt', (receipt) => {
-            console.log('Transacción completada:', receipt);
-            updateStats();
-        })
-        .on('error', (error) => {
-            console.error('Error al depositar:', error);
-        });
+// Función para depositar en el contrato
+function deposit(amount) {
+  contract.methods.deposit().send({value: amount}).then(function() {
+    console.log("Deposited " + amount + " wei");
+  });
 }
 
-async function withdraw() {
-    const amount = document.getElementById('withdrawAmount').value;
-    const accounts = await web3.eth.getAccounts();
-
-    contract.methods.withdraw(web3.utils.toWei(amount, 'ether')).send({ from: accounts[0] })
-        .on('transactionHash', (hash) => {
-            console.log('Transacción enviada:', hash);
-        })
-        .on('receipt', (receipt) => {
-            console.log('Transacción completada:', receipt);
-            updateStats();
-        })
-        .on('error', (error) => {
-            console.error('Error al retirar:', error);
-        });
+// Función para retirar del contrato
+function withdraw(amount) {
+  contract.methods.withdraw(amount).send().then(function() {
+    console.log("Withdrew " + amount + " wei");
+  });
 }
 
-async function claimReward() {
-    const accounts = await web3.eth.getAccounts();
-
-    contract.methods.claimReward().send({ from: accounts[0] })
-        .on('transactionHash', (hash) => {
-            console.log('Transacción enviada:', hash);
-        })
-        .on('receipt', (receipt) => {
-            console.log('Transacción completada:', receipt);
-            updateStats();
-        })
-        .on('error', (error) => {
-            console.error('Error al reclamar recompensa:', error);
-        });
+// Función para reclamar recompensas del fondo de tesorería
+function claimRewards() {
+  contract.methods.claimRewards().send().then(function() {
+    console.log("Rewards claimed");
+  });
 }
 
-async function bid() {
-    const amount = document.getElementById('bidAmount').value;
-    const accounts = await web3.eth.getAccounts();
-
-    contract.methods.bid(web3.utils.toWei(amount, 'ether')).send({ from: accounts[0] })
-        .on('transactionHash', (hash) => {
-            console.log('Transacción enviada:', hash);
-        })
-        .on('receipt', (receipt) => {
-            console.log('Transacción completada:', receipt);
-            updateStats();
-        })
-        .on('error', (error) => {
-            console.error('Error al realizar oferta en subasta:', error);
-        });
+// Función para obtener el saldo actual del fondo de tesorería
+function getTreasuryPool() {
+  contract.methods.treasuryPool().call().then(function(result) {
+    console.log("Treasury pool balance: " + result);
+  });
 }
 
-async function connectWallet() {
-    try {
-        await window.ethereum.request({ method: 'eth_requestAccounts' });
-        updateStats();
-    } catch (error) {
-        console.error('Error al conectar la cartera:', error);
-    }
+// Función para obtener la suma total de depósitos realizados por todos los usuarios
+function getTotalDeposits() {
+  contract.methods.totalDeposits().call().then(function(result) {
+    console.log("Total deposits: " + result);
+  });
 }
-
-if (window.ethereum) {
-    window.ethereum.on('accountsChanged', (accounts) => {
-        updateStats();
-    });
-} else {
-    console.error('No se encontró un proveedor de Ethereum. Por favor, instale MetaMask o use un navegador compatible.');
-}
-
-connectWallet();
